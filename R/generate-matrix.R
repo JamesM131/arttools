bef2e6ae757fd9df5d02b57761cf9f270754329a
James Monks <jamesmonks131@gmail.com>
2021-08-15 00:04:07 +1000

    Add generate_matrix function

---
+++ R/generate-matrix.R
@@ -0,0 +1,33 @@
+#' Generate a forces matrix
+#'
+#' Create a forces matrix based on mathematical expressions.
+#' Taking x and y, evaluating force z.
+#'
+#' @param exp Mat expression.
+#' @param size Number of rows/columns to create.
+#' @param scale Whether to apply scaling.
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' # ADD_EXAMPLES_HERE
+generate_matrix <- function(exp, size = 80, scale = FALSE) {
+    half_size <- size / 2
+    exp <- rlang::enexpr(exp)
+
+    series_up <- scale(-half_size:(half_size - 1))
+    series_down <- scale((half_size - 1):(-half_size))
+
+    matrix <- cross2(series_up, series_down) %>%
+        map_dbl(~ {
+            x <- .x[[1]]
+            y <- .x[[2]]
+            z <- rlang::eval_bare(exp)
+            return(z)
+        }) %>%
+        matrix(nrow = size)
+
+    if (scale) {
+        matrix <- (matrix - mean(matrix)) / diff(range(matrix))
+    }
+    return(matrix)
+}
